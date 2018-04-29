c490eda00b5f04e31689f5825cc2621bfb7e405a
UAHollow <uahollow@gmail.com>
2018-04-29 20:27:40 +0300

    Implemented processing of multiple arguments and empty arguments check-up. Removed unneeded if.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,27 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String textLine;
+        int counter = 0;
+        for(String i:args){
+            if (i.isEmpty()){
+                counter += 1;
+            }
+        }
+
+        if (counter == args.length){
+            System.out.println("there is no arguments to work with");
+            return;
+        }
 
         while (scanner.hasNextLine()) {
             textLine = scanner.nextLine();
-            if (textLine.toLowerCase().contains(args[0].toLowerCase())) {
-                System.out.print(textLine + "\n"); }
-            if (scanner.nextLine().isEmpty()){
-                scanner.close();
-                break;
+            for(String i:args) {
+                if (i.isEmpty() || i.equals(" ")){
+                    continue;
+                }
+                if (textLine.toLowerCase().contains(i.toLowerCase())) {
+                    System.out.print(textLine + "\n");
+                }
             }
         }
     }
