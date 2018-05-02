0c0cbd60629e97da16af14230538fa3b46c59928
UAHollow <uahollow@gmail.com>
2018-05-02 08:34:06 +0300

    moved variable intializations to where they're used. added some spacebars in beteen brackets.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,24 +6,22 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("To exit press Ctrl+D");
-        String trimmedArg;
         List<String> validArgs = new LinkedList<>();
-        for(String unvalidatedArgs:args){
-            trimmedArg = unvalidatedArgs.trim();
-            if (!trimmedArg.isEmpty()){
+        for(String unvalidatedArgs:args) {
+            String trimmedArg = unvalidatedArgs.trim();
+            if (!trimmedArg.isEmpty()) {
                 validArgs.add(trimmedArg);
             }
         }
 
-        if (validArgs.size() == 0){
+        if (validArgs.size() == 0) {
             System.out.println("there is no arguments to work with. Bye!");
             return;
         }
 
         Scanner scanner = new Scanner(System.in);
-        String textLine;
         while (scanner.hasNextLine()) {
-            textLine = scanner.nextLine();
+            String textLine = scanner.nextLine();
             for(String vArgs:validArgs) {
                 if (textLine.toLowerCase().contains(vArgs.toLowerCase())) {
                     System.out.println(textLine);
